4177004f5e4707b147c292e5754ce97f5961a139
joao96 <jvpoletti@gmail.com>
2021-10-15 16:53:50 -0300

    ♻️ refactoring useCase for categories

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -9,8 +9,10 @@ class CreateCategoryUseCase {
   // ja cria o atributo, bem como o inicializa com o valor passado ao constructor
   constructor(private categoriesRepository: ICategoriesRepository) {}
 
-  execute({ name, description }: IRequest): void {
-    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+  async execute({ name, description }: IRequest): Promise<void> {
+    const categoryAlreadyExists = await this.categoriesRepository.findByName(
+      name
+    );
 
     if (categoryAlreadyExists) {
       throw new Error('Category already exists.');
