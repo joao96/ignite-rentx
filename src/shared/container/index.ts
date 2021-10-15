26ccefe8cb63159352e01ba270ae41ad71f26738
joao96 <jvpoletti@gmail.com>
2021-10-15 20:08:47 -0300

    ♻️ refact specifications + list, import categories

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,9 +2,17 @@ import { container } from 'tsyringe';
 
 import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
 import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
+import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
+import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
 
 // ICategoriesRepository
 container.registerSingleton<ICategoriesRepository>(
   'CategoriesRepository', // nome do container que esta sendo criado (vai referenciar a classe CategoriesRepository)
   CategoriesRepository // a classe que sera chamada toda vez que o nome anterior for referenciado
+);
+
+// ISpecificationsRepository
+container.registerSingleton<ISpecificationsRepository>(
+  'SpecificationsRepository',
+  SpecificationsRepository
 );
