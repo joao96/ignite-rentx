38f6fbbe69c29cc83f43285574cd803facb5217e
joao96 <jvpoletti@gmail.com>
2021-10-16 00:37:00 -0300

    ✨ creating user controller and adding routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specifications.routes';
+import { usersRoutes } from './users.routes';
 
 const router = Router();
 
 // todas as reqs que comecarem por '/categories' irao passar pelo middleware categoriesRoutes
 router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationsRoutes);
+router.use('/users', usersRoutes);
 
 export { router };
