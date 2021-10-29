d88835910f1cedc2ed42184b95dc8d7b5ce37154
joao96 <jvpoletti@gmail.com>
2021-10-29 16:21:11 -0300

    🗃️ migration for rentals table

--- src/shared/infra/typeorm/migrations/1634353054844-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1634353054844-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1634353054844 implements MigrationInterface {
           {
             name: 'id',
             type: 'uuid',
+            isPrimary: true,
           },
           {
             name: 'name',
