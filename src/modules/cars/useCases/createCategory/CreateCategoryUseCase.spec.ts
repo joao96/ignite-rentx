69bdc34ead9d5f624996cd3cb8f55a5bd4dfd698
joao96 <jvpoletti@gmail.com>
2021-11-03 16:09:44 -0300

    ✅ adjusting expect for all tests

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -32,22 +32,22 @@ describe('Create Category', () => {
     expect(categoryCreated).toHaveProperty('id');
   });
 
-  it('Should not be able to create a new category with used name', async () => {
-    expect(async () => {
-      const category = {
-        name: 'Category Test',
-        description: 'Category Description Test',
-      };
+  it('Should not be able to create a new category with a used name', async () => {
+    const category = {
+      name: 'Category Test',
+      description: 'Category Description Test',
+    };
 
-      await createCategoryUseCase.execute({
+    await createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description,
+    });
+
+    await expect(
+      createCategoryUseCase.execute({
         name: category.name,
         description: category.description,
-      });
-
-      await createCategoryUseCase.execute({
-        name: category.name,
-        description: category.description,
-      });
-    }).rejects.toBeInstanceOf(AppError);
+      })
+    ).rejects.toEqual(new AppError('Category already exists.'));
   });
 });
